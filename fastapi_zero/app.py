cb134e71a385b00e0be7b453efb038a438263e74
Joao Guedes <dev.jguedes@gmail.com>
2025-11-03 10:48:32 -0300

    First commit

---
+++ fastapi_zero/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi_zero.schemas import Message
+
+app = FastAPI()
+
+
+@app.get('/' status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
+    return {'message': 'Olá Mundo!'}

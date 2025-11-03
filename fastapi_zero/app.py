75926981d82d0d480853610d5f13c32fb46c0da4
Joao Guedes <dev.jguedes@gmail.com>
2025-11-03 11:25:10 -0300

    test/add my first test

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
 from http import HTTPStatus
+
+from fastapi import FastAPI
 
 from fastapi_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/' status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
